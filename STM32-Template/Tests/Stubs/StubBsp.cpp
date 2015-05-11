b83fa9573b7d9e65e6b7ebc8ab8e785f345bc9c3
cbartlett <cbartlet@uw.edu>
2015-05-11 12:33:17 -0700

    Fix broken tests.

--- STM32-Template/Tests/Stubs/StubBsp.cpp
+++ STM32-Template/Tests/Stubs/StubBsp.cpp
@@ -10,10 +10,10 @@
  #define SYSCLK_FREQ_24MHz  24000000
 uint32_t SystemCoreClock = SYSCLK_FREQ_24MHz;
 
-App::CSStatus_t App::CSLock(int& lock)
+App::CSStatus_t App::CSLock(int* lock)
 {
-	App::CSStatus_t temp = static_cast<App::CSStatus_t>(lock);
-	lock = App::LOCKED_STATE;
+	App::CSStatus_t temp = static_cast<App::CSStatus_t>(*lock);
+	*lock = App::LOCKED_STATE;
 
     return temp;
 }
