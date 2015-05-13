ff4376a16a419420c1af550569a2679891f8a495
cbartlett <cbartlet@uw.edu>
2015-05-13 16:20:05 -0700

    Fix compilation errors.

--- STM32-Template/Tests/Stubs/StubBsp.cpp
+++ STM32-Template/Tests/Stubs/StubBsp.cpp
@@ -10,10 +10,10 @@
  #define SYSCLK_FREQ_24MHz  24000000
 uint32_t SystemCoreClock = SYSCLK_FREQ_24MHz;
 
-App::CSStatus_t App::CSLock(int* lock)
+Bsp::CSStatus_t Bsp::CSLock(int* lock)
 {
-	App::CSStatus_t temp = static_cast<App::CSStatus_t>(*lock);
-	*lock = App::LOCKED_STATE;
+	Bsp::CSStatus_t temp = static_cast<Bsp::CSStatus_t>(*lock);
+	*lock = Bsp::LOCKED_STATE;
 
     return temp;
 }
